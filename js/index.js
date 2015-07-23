c355a562823b84a8aa4fac35bcee23d6b289b65a
daniel_zepp <s_u_n_e@naver.com>
2015-07-23 18:05:46 +0900

    no message

--- js/index.js
+++ js/index.js
@@ -59,6 +59,14 @@ $(function()
 	{
 	    $(this).children().stop().fadeOut(300);
 	});
+
+	$('#after_signin').hover(function ()
+	{
+	    $('#after_signin > ul').fadeIn();
+	}, function ()
+	{
+	    $('#after_signin > ul').fadeOut();
+	});
 });
 
 $(function () { //search box click value reset start
