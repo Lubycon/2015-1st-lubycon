fc296ad711e49d80a452bb97dec2cca92a364445
Dart_DeskTop <bboydart91@gmail.com>
2015-07-27 01:44:17 +0900

    added draw_circle function in success_account page
    Former-commit-id: 466f3669a721cbe7d28db2c12b74110cc5324789

--- js/success_account.js
+++ js/success_account.js
@@ -1,18 +1,42 @@
 
 $(function(){
-	//first animation start
-	$("#thanks").animate({opacity:1},1000);//1s	
-	$("#circle").animate({opacity:1},1000);//1s
-	//first animation end
+	////////////////////////////////////////////first animation start
+	$("#thanks").animate({opacity:1},700);//1s	
+	$("#circle").animate({opacity:1},700);//1s
+    //draw circle start
+    $(function(){
+        var context = $('#cir_body')[0].getContext('2d');
+		var dig1 = 360/3000;
+		var numalt = (dig1*$('#cir_body').text());
+		var i = 0;
+		
+		context.translate(65,65);//center
+		context.rotate(315*(Math.PI/90));//start point and draw speed
+		
+		setInterval(function(){
+			context.beginPath();
+			if( i > -numalt ){
+				context.arc(0 ,0 ,50 ,0 ,i*(Math.PI/60) ,true);
+				i--;
+			};
+				
+			context.strokeStyle = '#8ec89a';
+			context.lineWidth = 100;
+			context.stroke();
+		},2);
+	});
+				
+	//draw circle end
+	/////////////////////////////////////////////first animation end
 	$("#thanks").queue(function(){
-		//second animation start					
-		$("#thanks2").animate({opacity:1},1000);//1s
-		//second animation end		
+		/////////////////////////////////////////second animation start					
+		$("#thanks2").animate({opacity:1},700);//1s
+		/////////////////////////////////////////second animation end		
 		$("#thanks2").queue(function(){
-			//third animation start			
+			////////////////////////////////////third animation start			
 			setTimeout("go_index()", 4000);//4s
 			setInterval("remove_rect()", 1000);//1s
-			//third animation end		
+			///////////////////////////////////third animation end		
 		});
 	});
 });
@@ -20,11 +44,11 @@ $(function(){
 
 
 
-	function go_index(){
-			location.href = "index.html";
-	};
+function go_index(){
+		location.href = "index.html";
+};
 
-	function remove_rect(){
-		$(".timer_rect:last").removeClass("timer_rect")
-	};
+function remove_rect(){
+	$(".timer_rect:last").removeClass("timer_rect")
+};
 
