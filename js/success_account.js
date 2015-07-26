7646f2bf385952337b1f5a81e8fbee848e20ca19
Dart_DeskTop <bboydart91@gmail.com>
2015-07-26 21:33:38 +0900

    added animation in success_account page
    Former-commit-id: 2a4e6b7c246c430c666bde8f1cc5492fa2f9421c

--- js/success_account.js
+++ js/success_account.js
@@ -1,14 +1,30 @@
-//success_account page timer start
-$(document).ready(function() {
-	setTimeout("go_index()", 4000);
-	setInterval("remove_rect()", 1000); 
+
+$(function(){
+	//first animation start
+	$("#thanks").animate({opacity:1},1000);//1s	
+	$("#circle").animate({opacity:1},1000);//1s
+	//first animation end
+	$("#thanks").queue(function(){
+		//second animation start					
+		$("#thanks2").animate({opacity:1},1000);//1s
+		//second animation end		
+		$("#thanks2").queue(function(){
+			//third animation start			
+			setTimeout("go_index()", 4000);//4s
+			setInterval("remove_rect()", 1000);//1s
+			//third animation end		
+		});
+	});
 });
 
-function go_index(){
-		location.href = "index.html";
-};
 
-function remove_rect(){
-	$(".timer_rect:last").removeClass("timer_rect")
-};
-//success_account page timer end
+
+
+	function go_index(){
+			location.href = "index.html";
+	};
+
+	function remove_rect(){
+		$(".timer_rect:last").removeClass("timer_rect")
+	};
+
