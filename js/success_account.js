c5c1756fcda272fa8341f17a15c1425f6e62864e
Dart_DeskTop <bboydart91@gmail.com>
2015-07-27 14:49:50 +0900

    encapsulation draw circle function in success_account.js
    Former-commit-id: 316195d2a80b6a16f154ced692d40b5a8ad32784

--- js/success_account.js
+++ js/success_account.js
@@ -3,30 +3,7 @@ $(function(){
 	////////////////////////////////////////////first animation start
 	$("#thanks").animate({opacity:1},700);//1s	
 	$("#circle").animate({opacity:1},700);//1s
-    //draw circle start
-    $(function(){
-        var context = $('#cir_body')[0].getContext('2d');
-		var dig1 = 360/3000;
-		var numalt = (dig1*$('#cir_body').text());
-		var i = 0;
-		
-		context.translate(65,65);//center
-		context.rotate(315*(Math.PI/90));//start point and draw speed
-		
-		setInterval(function(){
-			context.beginPath();
-			if( i > -numalt ){
-				context.arc(0 ,0 ,50 ,0 ,i*(Math.PI/60) ,true);
-				i--;
-			};
-				
-			context.strokeStyle = '#8ec89a';
-			context.lineWidth = 100;
-			context.stroke();
-		},2);
-	});
-				
-	//draw circle end
+    draw_circle();			
 	/////////////////////////////////////////////first animation end
 	$("#thanks").queue(function(){
 		/////////////////////////////////////////second animation start					
@@ -48,7 +25,32 @@ function go_index(){
 		location.href = "index.html";
 };
 
+
+
 function remove_rect(){
 	$(".timer_rect:last").removeClass("timer_rect")
 };
 
+
+
+function draw_circle(){
+	var context = $('#cir_body')[0].getContext('2d');
+	var dig1 = 360/3000;
+	var numalt = 3600;
+	var i = 0;
+		
+	context.translate(65,65);//center
+	context.rotate(315*(Math.PI/90));//start point and draw speed
+	
+	setInterval(function(){
+		context.beginPath();
+		if( i > -numalt ){
+			context.arc(0 ,0 ,50 ,0 ,i*(Math.PI/60) ,true);
+			i--;
+		};
+			
+		context.strokeStyle = '#8ec89a';
+		context.lineWidth = 100;
+		context.stroke();
+	},2);
+};
