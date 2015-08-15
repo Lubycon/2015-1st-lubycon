32a9591d336c2ba7701efe99447dd3d9989013d3
Dart_DeskTop <bboydart91@gmail.com>
2015-08-16 01:27:05 +0900

    90% finished tooltip in svg graph(added change data)
    Former-commit-id: 2d1c1b1ee4b5b13dc34be2acc9c9b92c2c7bd03b

--- d3/test_graph/test_graph.js
+++ d3/test_graph/test_graph.js
@@ -3,51 +3,63 @@
 var upload_data = [
     {
         "value": "15",
-        "month": "1"
+        "month": "1",
+        "name" : "January"
     }, 
     {
         "value": "22",
-        "month": "2"
+        "month": "2",
+        "name" : "February"
     }, 
     {
         "value": "33",
-        "month": "3"
+        "month": "3",
+        "name" : "March"
     }, 
     {
         "value": "5",
-        "month": "4"
+        "month": "4",
+        "name" : "April"
     }, 
     {
         "value": "34",
-        "month": "5"
+        "month": "5",
+        "name" : "May"
     }, 
     {
         "value": "22",
-        "month": "6"
+        "month": "6",
+        "name" : "June"
     },
     {
         "value": "7",
-        "month": "7"
+        "month": "7",
+        "name" : "July"
     },
     {
         "value": "6",
-        "month": "8"
+        "month": "8",
+        "name" : "August"
     },
     {
         "value": "1",
-        "month": "9"
+        "month": "9",
+        "name" : "September"
     },
     {
         "value": "7",
-        "month": "10"
+        "month": "10",
+        "name" : "October"
     },
     {
         "value": "11",
-        "month": "11"
+        "month": "11",
+        "name" : "November"
     },
     {
         "value": "3",
-        "month": "12"
+        "month": "12",
+        "name" : "December"
     }
 ];
 //upload data
@@ -63,8 +75,8 @@ var vis = d3.select('#graph'),
         left: 50
     },
 
-    xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([1,12]),
-    yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([0,50]),
+    xScale = d3.scale.linear().domain([1,12]).range([MARGINS.left, WIDTH - MARGINS.right]),
+    yScale = d3.scale.linear().domain([0,50]).range([HEIGHT - MARGINS.top, MARGINS.bottom]),
 
     xAxis = d3.svg.axis()
             .scale(xScale),
@@ -138,6 +150,7 @@ vis.selectAll("svg")
     .attr('cx', function(d,i) { return xScale(d.month); })
     .attr('cy', function(d,i) { return yScale(d.value); })
     .attr('r', 5)
+    .attr('index', function(d,i){ return i})
     .attr('fill', 'white')
     .attr('stroke', '#48cfad')
     .attr('stroke-width', '1')
@@ -147,19 +160,57 @@ vis.selectAll("svg")
         d3.select(this).transition()//animation for circle
             .delay(0)
             .duration(500)
-            .attr('r',8);
+            .attr('r',8);                     
 
-        var value_both = d3.select('div.value');
+            var value_both = d3.select('div.value');
 
-        value_both.selectAll("p")
-            .data(upload_data)
-            .enter().append("p")
-            .attr('class','value_text')
-            .html(function(d,i) {
-                return "month : "+ upload_data[0].month + '<br>' + "contents : " + upload_data[0].value;
+            value_both.selectAll("p")//p are too much. fix later//
+                .data(upload_data)
+                .enter().append("p")
+                .attr('class','value_text')
+            
+            //////////////////////////////////////////////tool_tip(jQuery)///////////////////////////////////////////////////////
+            var cv = $(this).attr('index');//circles index(jQuery)
+            
+            $('.value_text').html(function() {
+                if(cv==0){/////////////////////////////////////////////////////////////////// you should change to switch later//
+                    return "2015-January"+ '<br>' + "contents : " + upload_data[0].value;
+                }
+                else if(cv==1){
+                    return "2015-February"+ '<br>' + "contents : " + upload_data[1].value;
+                }
+                else if(cv==2){
+                    return "2015-March"+ '<br>' + "contents : " + upload_data[2].value;
+                }
+                else if(cv==3){
+                    return "2015-April"+ '<br>' + "contents : " + upload_data[3].value;
+                }
+                else if(cv==4){
+                    return "2015-May"+ '<br>' + "contents : " + upload_data[4].value;
+                }
+                else if(cv==5){
+                    return "2015-June"+ '<br>' + "contents : " + upload_data[5].value;
+                }
+                else if(cv==6){
+                    return "2015-July"+ '<br>' + "contents : " + upload_data[6].value;
+                }
+                else if(cv==7){
+                    return "2015-August"+ '<br>' + "contents : " + upload_data[7].value;
+                }
+                else if(cv==8){
+                    return "2015-September"+ '<br>' + "contents : " + upload_data[8].value;
+                }
+                else if(cv==9){
+                    return "2015-October"+ '<br>' + "contents : " + upload_data[9].value;
+                }
+                else if(cv==10){
+                    return "2015-November"+ '<br>' + "contents : " + upload_data[10].value;
+                }
+                else if(cv==11){
+                    return "2015-December"+ '<br>' + "contents : " + upload_data[11].value;
+                }
             });
 
-        ////////////////////////tool_tip(jQuery)/////////////////////////
         var tooltipX = $(this).offset().left + 10;///circle X = tooltip_X
         var tooltipY = $(this).offset().top + 10;////circle Y = tooltip_Y
 
@@ -167,7 +218,7 @@ vis.selectAll("svg")
         $('#tooltip').css('left',tooltipX);
 
         $('#tooltip').stop().fadeIn(200);
-        ////////////////////////tool_tip(jQuery)/////////////////////////
+        ///////////////////////////////////////////////////tool_tip(jQuery)/////////////////////////////////////////////////////
     })
 
     
