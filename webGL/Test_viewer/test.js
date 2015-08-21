f8c8df2ff0cb2f26167d54d5dc6452dce62f8e61
Dart_DeskTop <bboydart91@gmail.com>
2015-08-21 22:27:38 +0900

    created 3D object viewer test version
    Former-commit-id: 949a5beb3d35d0414d0437deb04730a3c77d4032

---
+++ webGL/Test_viewer/test.js
@@ -0,0 +1,36 @@
+var scene = new THREE.Scene();
+			var camera = new THREE.PerspectiveCamera( 50, window.innerWidth/window.innerHeight, 0.1, 1000 );
+			//(field of view,aspect{width/height},near,far) Perspective Projection
+
+			var renderer = new THREE.WebGLRenderer();//renderer start
+			var controls = new THREE.OrbitControls( camera, renderer.domElement );//orbit controller on
+			
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
+
+			var geometry = new THREE.SphereGeometry(1,20,20);//shape
+			var geometry2 = new THREE.BoxGeometry(3,3,3);
+			var material = new THREE.MeshLambertMaterial({color: 0xffbe54, wireframe: false});//texture
+			var material2 = new THREE.MeshBasicMaterial({color: 0x0000ff, wireframe: true});
+			
+			var cir1 = new THREE.Mesh(geometry, material);
+			var cir2 = new THREE.Mesh(geometry2, material2);
+			scene.add(cir1);
+			scene.add(cir2);
+
+
+			var Light = new THREE.DirectionalLight(0xffffff,1);//(color, intensity, distance)
+			Light.position.set(1, 1, 1); // Position the light source at (x, y, z).
+			scene.add(Light); // Add the light source to the scene.
+			
+			camera.position.z = 6;//z index
+
+			var render = function () {//cube animation start
+				requestAnimationFrame( render );
+
+				cir1.rotation.y += 0.01;			
+				cir2.rotation.y += 0.01;
+
+				renderer.render(scene, camera);
+			};
+			render();
