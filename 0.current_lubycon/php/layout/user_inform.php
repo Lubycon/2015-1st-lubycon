00708188865ceb98aa84676214f99593e89a8de0
daniel_zepp poterble <s_u_n_e@naver.com>
2015-08-29 18:21:54 +0900

    personal page modul
    Former-commit-id: 019a7a8b5bf636f13ddbf84d78149272105b9bcd

---
+++ 0.current_lubycon/php/layout/user_inform.php
@@ -0,0 +1,46 @@
+<div id="information_body">
+        <div id="information_inbody">
+            <section id="personal_info">
+                <figure id="personal_photo">
+                    <img src="ch/img/designer_img.png" alt="Designer_photo" />
+                </figure>
+                
+                <div id="basic_info_box">
+                    <span id="personal_name">Daniel Zepp</span>
+                    <span id="personal_job">Front End Engineer</span>
+                    <p id="personal_legion"><i class="fa fa-home"></i>Seoul, Korea</p>
+                    <p id="personal_language"><i class="fa fa-language"></i>English, Korean</p>
+                </div>
+                <div id="descript_box">
+                    <p id="personal_des">
+                        Hi Lubycon guys. I'm Daniel.<br>
+                        I work at front-end engineer and usally use Javascript or PHP. 
+                        If you felt something after see my design that please message to me right now :)<!--166byte/space-->
+                    </p>
+                </div>
+            </section>
+            <section id="personal_contact">
+                <ul>
+                    <li class="personal_num">
+                        <i class="fa fa-mobile"></i><span class="in_num">+82-10-2947-2929</span>
+                    </li>
+                    <li class="personal_num">
+                        <i class="fa fa-fax"></i><span class="in_num">+82-2-6997-2828</span>
+                    </li>
+                    <li class="personal_num">
+                        <i class="fa fa-desktop"></i><span class="in_num"><a href="#">www.lubycon.com</a></span>
+                    </li>
+                </ul>
+            </section>
+            <section id="personal_score">
+                <ul>
+                    <li><i class="fa fa-eye"></i><p>123</p></li>
+                    <li><i class="fa fa-heart"></i><p>111123</p></li>
+                    <li><i class="fa fa-download"></i><p>123</p></li>
+                    <li><i class="fa fa-user-plus"></i><p>123</p></li>
+                </ul>
+                <button id="personal_messeage"><i class="fa fa-envelope-o"></i><p>Messeage</p></button>
+                <button id="personal_follow"><i class="fa fa-user-plus"></i><p>Follow</p></button>
+            </section>
+        </div>
+    </div>
