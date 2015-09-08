1e29a0de3fdc297456876e5c07beb0f919f7326a
Dart_Mac <bboydart91@gmail.com>
2015-09-08 15:19:20 +0900

    added community write page in onlyHTML
    Former-commit-id: e0eeffaa7ed50e34370ee7b39ec57285a3a779e7

---
+++ 0.current_lubycon/php/community/community_write.php
@@ -0,0 +1,34 @@
+﻿<figure id="main_figure">
+    <h2>COMMUNITY</h2>
+</figure>	<!-- end main_figure -->
+
+<link href="css/community_write.css" rel="stylesheet" type="text/css" />  <!-- community_view css -->
+<link href="css/community.css" rel="stylesheet" type="text/css" />  <!-- community css -->
+<script type="text/javascript" src="js/community.js"></script>
+
+<section id="contents">
+    <section id="navsel">
+        <nav id="lnb_nav">
+            <ul>
+                <li class="selected_nav">Recent</li>
+                <li>Most Like</li>
+                <li>Most Comment</li>
+            </ul>
+        </nav>  <!-- end lnb nav -->
+        <div class="search_option">
+            <i class="fa fa-filter"></i>
+            <select class="basic_filter">
+                <option value="English">English</option>
+                <option value="Korean">Korean</option>
+                <option value="Japanese">Japanese</option>
+                <option value="Chinese">Chinese</option>
+                <option value="French">French</option>
+            </select>
+
+        </div>  <!-- end select box -->
+    </section>
+
+    <section id="post_subject_area">  
+    </section>  <!-- end nav_guide -->
+    
+</section>  <!-- end contents section -->
