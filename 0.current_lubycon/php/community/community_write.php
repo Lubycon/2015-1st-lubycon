98b93404e2aced381356cad11c123e44bd145726
Dart_Mac <bboydart91@gmail.com>
2015-09-11 15:59:01 +0900

    reset to community_write page
    Former-commit-id: bf5555483a3e5a85c8ceec40211249d0c80eb8f6

--- 0.current_lubycon/php/community/community_write.php
+++ 0.current_lubycon/php/community/community_write.php
@@ -4,7 +4,7 @@
 
 <link href="css/community_write.css" rel="stylesheet" type="text/css" />  <!-- community_view css -->
 <link href="css/community.css" rel="stylesheet" type="text/css" />  <!-- community css -->
-<link href="css/quill_basic.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="js/community.js"></script>
 
 
 <section id="contents">
@@ -44,101 +44,107 @@
         
         <div class="properties_box">
             <p class="work_title">Contents</p>
-            <div id="main_work_space">                   
-                <div id="content-container">
-                    <div id="editor-wrapper">
-                        <div id="formatting-container">
-                            <select title="Font" class="ql-font basic_filter">
-                                <option value="sans-serif" selected>Sans Serif</option>
-                                <option value="Georgia, serif">Serif</option>
-                                <option value="Monaco, 'Courier New', monospace">Monospace</option>
+            
+            <div id="main_work_space">
+                <div id="write_tool_box">
+                    <span class="tool_set_com" id="font_prop_com">
+                        <span class="tool_com" id="font_fam_com">
+                            <select class="basic_filter">
+                                <option value="Gulim">Gulim</option>
+                                <option value="Dotum">Dotum</option>
                             </select>
-                            <select title="Size" class="ql-size basic_filter">
-                                <option value="10px">Small</option>
-                                <option value="13px" selected>Normal</option>
-                                <option value="18px">Large</option>
-                                <option value="32px">Huge</option>
+                            <span class="tooltip_bt" id="ff_com_tip">Font</span><!--tooltip-->
+                        </span><!--font famaily-->
+                        <span class="tool_com" id="font_size_com">
+                            <select class="basic_filter">
+                                <option value="14pt">14pt</option>
+                                <option value="18pt">18pt</option>
+                                <option value="24pt">24pt</option>
+                                <option value="30pt">30pt</option>
+                                <option value="36pt">36pt</option>
+                                <option value="48pt">48pt</option>
+                                <option value="60pt">60pt</option>
+                                <option value="72pt">72pt</option>
                             </select>
-                            <select title="Text Color" class="ql-color basic_filter">
-                                <option value="rgb(255, 255, 255)">White</option>
-                                <option value="rgb(0, 0, 0)" selected>Black</option>
-                                <option value="rgb(255, 0, 0)">Red</option>
-                                <option value="rgb(0, 0, 255)">Blue</option>
-                                <option value="rgb(0, 255, 0)">Lime</option>
-                                <option value="rgb(0, 128, 128)">Teal</option>
-                                <option value="rgb(255, 0, 255)">Magenta</option>
-                                <option value="rgb(255, 255, 0)">Yellow</option>
-                            </select>
-                            <select title="Background Color" class="ql-background basic_filter">
-                                <option value="rgb(255, 255, 255)" selected>White</option>
-                                <option value="rgb(0, 0, 0)">Black</option>
-                                <option value="rgb(255, 0, 0)">Red</option>
-                                <option value="rgb(0, 0, 255)">Blue</option>
-                                <option value="rgb(0, 255, 0)">Lime</option>
-                                <option value="rgb(0, 128, 128)">Teal</option>
-                                <option value="rgb(255, 0, 255)">Magenta</option>
-                                <option value="rgb(255, 255, 0)">Yellow</option>
-                            </select>
-                            <select title="Text Alignment" class="ql-align basic_filter">
-                                <option value="left" selected>
-                                    <i class="fa fa-align-left"></i>
-                                    Left</option>
-                                <option value="center">
-                                    <i class="fa fa-align-center"></i>
-                                    Center</option>
-                                <option value="right">
-                                    <i class="fa fa-align-right"></i>
-                                    Right</option>
-                                <option value="justify">
-                                    <i class="fa fa-align-justice"></i>
-                                    Justify</option>
-                            </select>
-                            <button title="Bold" class="ql-format-button ql-bold">
-                                <i class="fa fa-bold"></i>                               
-                            </button>
+                            <span class="tooltip_bt" id="fs_com_tip">Font Size</span><!--tooltip-->
+                        </span><!--font size-->
+                    </span>
+                    <span class="tool_set_com" id="font_deco_com">
+                        <span class="tool_com">
+                            <i class="fa fa-bold"></i>
                             <span class="tooltip_bt" id="bold_com_tip">Bold</span><!--tooltip-->
-
-                            <button title="Italic" class="ql-format-button ql-italic">
-                                <i class="fa fa-italic"></i>
-                            </button>
-                            <span class="tooltip_bt" id="italic_com_tip">Italic</span>
-                            
-                            <button title="Underline" class="ql-format-button ql-underline">
-                                <i class="fa fa-underline"></i>
-                            </button>
-                            <span class="tooltip_bt" id="underline_com_tip">Underline</span>
-                            
-                            <button title="Strikethrough" class="ql-format-button ql-strike">
-                                <i class="fa fa-strikethrough"></i>
-                            </button>
-                            <span class="tooltip_bt" id="strike_com_tip">Strike</span>                           
-                            
-                            <button title="Link" class="ql-format-button ql-link">
-                                <i class="fa fa-link"></i>
-                            </button>
-                            <span class="tooltip_bt" id="link_com_tip">Link</span>
-                            
-                            <button title="Image" class="ql-format-button ql-image">
-                                <i class="fa fa-picture-o"></i>
-                            </button>
-                            <span class="tooltip_bt" id="img_com_tip">Embed Image</span>
-                            
-                            <button title="Ul" class="ql-format-button ql-bullet">
-                                <i class="fa fa-list-ul"></i>
-                            </button>
-                            <span class="tooltip_bt" id="ul_com_tip">Ul</span>
-                            
-                            <button title="Ol" class="ql-format-button ql-list">
-                                <i class="fa fa-list-ol"></i>
-                            </button>
-                            <span class="tooltip_bt" id="ol_com_tip">Ol</span>
-                        
-                        </div>
-                        <div id="editor-container"></div><!--input window-->
-                    </div><!--editor-wrapper end-->
-                </div><!--content-container end-->
+                        </span><!--bold-->
+                        <span class="tool_com">
+                            <i class="fa fa-italic"></i>
+                            <span class="tooltip_bt" id="italic_com_tip">Italic</span><!--tooltip-->
+                        </span><!--italic-->
+                        <span class="tool_com">
+                            <i class="fa fa-underline"></i>
+                            <span class="tooltip_bt" id="underline_com_tip">Underline</span><!--tooltip-->
+                        </span><!--underline-->
+                        <span class="tool_com">
+                            <i class="fa fa-strikethrough"></i>
+                            <span class="tooltip_bt" id="strike_com_tip">Strike</span><!--tooltip-->
+                        </span><!--strike-->
+                    </span>
+                    <span class="tool_set_com" id="text_align_com">
+                        <span class="tool_com">
+                            <i class="fa fa-align-left"></i>
+                            <span class="tooltip_bt" id="left_com_tip">Left</span><!--tooltip-->
+                        </span><!--left-->
+                        <span class="tool_com">
+                            <i class="fa fa-align-center"></i>
+                            <span class="tooltip_bt" id="center_com_tip">Center</span><!--tooltip-->
+                        </span><!--center-->
+                        <span class="tool_com">
+                            <i class="fa fa-align-right"></i>
+                            <span class="tooltip_bt" id="right_com_tip">Right</span><!--tooltip-->
+                        </span><!--right-->
+                    </span>
+                    <span class="tool_set_com" id="font_color_com">
+                        <span class="tool_com">
+                            <i class="fa fa-square"></i>
+                            <span class="tooltip_bt" id="fc_com_tip">Text Color</span><!--tooltip-->
+                        </span><!--color-->
+                        <span class="tool_com">
+                            <i class="fa fa-square"></i>
+                            <span class="tooltip_bt" id="fbg_com_tip">Text Background</span><!--tooltip-->
+                        </span><!--text background-->
+                    </span>
+                    <span class="tool_set_com" id="list_com">
+                        <span class="tool_com">
+                            <i class="fa fa-list-ul"></i>
+                            <span class="tooltip_bt" id="ul_com_tip">Ul</span><!--tooltip-->
+                        </span><!--ul-->
+                        <span class="tool_com">
+                            <i class="fa fa-list-ol"></i>
+                            <span class="tooltip_bt" id="ol_com_tip">Ol</span><!--tooltip-->
+                        </span><!--ol-->
+                    </span>
+                    <span class="tool_set_com" id="table_com">
+                        <span class="tool_com">
+                            <i class="fa fa-table"></i>
+                            <span class="tooltip_bt" id="table_com_tip">Insert Table</span><!--tooltip-->
+                        </span><!--table-->
+                    </span>
+                    <span class="tool_set_com" id="embed_com">
+                        <span class="tool_com">
+                            <i class="fa fa-picture-o"></i>
+                            <span class="tooltip_bt" id="img_com_tip">Embed Image</span><!--tooltip-->
+                        </span><!--insert img-->
+                        <span class="tool_com">
+                            <i class="fa fa-link"></i>
+                            <span class="tooltip_bt" id="link_com_tip">Embed Link</span><!--tooltip-->
+                        </span><!--embed media-->
+                        <span class="tool_com">
+                            <i class="fa fa-unlink"></i>
+                            <span class="tooltip_bt" id="unlink_com_tip">Unlink</span><!--tooltip-->
+                        </span><!--unlink-->
+                    </span>
+                </div><!--write_tool_box_end-->
+                <textarea id="insert_text_window"></textarea><!--insert text window-->
             </div><!--main_work_space end-->
-        </div><!--textbox properties end-->
+        </div><!--main textbox end-->
         
         <div class="properties_box">
             <p class="work_title">Link1</p>
@@ -148,49 +154,11 @@
             <p class="work_title">Link2</p>
             <input type="text" class="write_box_com" id="write_box_title" />
         </div><!--link2 end-->
-        <div class="properties_box" id="file_up_com">
+        <div class="properties_box">
             <p class="work_title">File Upload</p>
             <span id="file_import_bt">Find the file</span>
             <input type="file" id="file_import_com" />
             <input type="text" id="file_text_com" val="please select file..." readonly />
         </div><!--link2 end-->
-    </section><!--work_space end-->
-    
-    <script type="text/javascript" src="js/quill.min.js"></script>
-    <script type="text/javascript">
-      var editor = new Quill('#editor-container', {
-        modules: {
-          'toolbar': { container: '#formatting-container' },
-          'authorship': { authorId: 'galadriel', enabled: true },
-          'link-tooltip': true,
-          'image-tooltip': true,
-          'multi-cursor': true,
-        }
-      });
-      editor.on('selection-change', function(range) {
-        console.log('selection-change', range)
-      });
-      editor.on('text-change', function(delta, source) {
-        console.log('text-change', delta, source)
-      });
-
-        $(function(){
-            $('#file_import_bt').click(function () {
-                $('#file_import_com').click();
-            });
-        
-            $('#file_import_com').change(function () {
-                $('#file_text_com').val($(this).val());
-            });
-        });
-        
-        $(function () {
-            $('#formatting-container button').hover(function ()
-            {
-                $(this).next('.tooltip_bt').stop().fadeIn(300).css("display", "inline-block");
-            }, function () {
-                $(this).next('.tooltip_bt').stop().fadeOut(300);
-            });
-        });
-    </script>        
+    </section><!--work_space end-->        
 </section>  <!-- end contents section -->
