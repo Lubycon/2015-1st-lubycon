6d19e943adcd2ce533ce739d4ee8bebab30e5e65
daniel_zepp poterble <s_u_n_e@naver.com>
2015-12-06 19:45:22 +0900

    test file
    Former-commit-id: 7a0f91b9e08fa4cf3f072ce1126bacb22e80f62b

---
+++ 0.current_lubycon/php/editor/test.php
@@ -0,0 +1,37 @@
+<?php
+    $con_article = $_POST['contents_article'];
+    // it's for multiple select box
+    $sel_cate = $_POST['user_selected_category'];
+    $sel_tag = $_POST['user_selected_tag'];
+    //
+
+    echo "contents_subject = " . $_POST['contents_subject'];
+    if($con_article)
+    {
+        for($k=0 ; $k< count($con_article); $k++)
+        {
+            echo "<br/>contents article".$k."=";
+            echo $con_article[$k];
+        };
+    };
+
+    if($sel_cate)
+    {
+        echo "<br/>user selectd categories = ";
+        for($i=0 ; $i< count($sel_cate); $i++)
+        {
+            echo $sel_cate[$i] . " ";
+        };
+    };
+    if($sel_tag)
+    {
+        echo "<br/>user selectd tags = ";
+        for($j=0 ; $j< count($sel_tag); $j++)
+        {
+            echo $sel_tag[$j] . " ";
+        };
+    };
+
+    echo "<br/>setting_desc = " . $_POST['setting_desc'];
+    echo "<br/>setting_price_option = " . $_POST['setting_price_option'];
+?>
