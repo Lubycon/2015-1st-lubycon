a2b38c49ee53021f629e70a4d82ebc992454eb65
Dart_Mac <bboydart91@gmail.com>
2015-12-18 16:12:32 +0900

    added artist_page.php and css
    Former-commit-id: 28a133fa34f23c0a9164beb1d85bfee64e523642

---
+++ 0.current_lubycon/php/layout/artist_card.php
@@ -0,0 +1,26 @@
+<li>
+    <?php
+        $username = "Admin_User";
+        $user_img_url = "./ch/img/no_img/no_img_user1.jpg";
+        $user_location_img = "./ch/img/flag_icons/United-States-Of-America.png";
+        $userjob = "Job";
+    ?><!--you should change to mySQL later-->
+    <div class="artists_card">
+        <div class="artist_top_info">
+            <div class="artist_pic">
+                <img src="<?=$user_img_url?>" alt="user_pic"/>
+            </div>
+            <div class="artist_location">
+                <img src="<?=$user_location_img?>" alt="user_location"/>
+            </div>
+        </div>
+        <div class="artist_mid_info">
+            <p class="artist_name"><?=$username?></p><!--user name-->
+            <p class="artist_job"><i class="fa fa-suitcase"></i><?=$userjob?></p><!--job-->
+        </div>
+        <div class="artist_bot_info">
+            <div class="view_profile_bt"><i class="fa fa-search-plus"></i>View Profile</div>
+        </div>
+        
+    </div>
+</li>
