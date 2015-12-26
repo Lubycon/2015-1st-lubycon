62fb2c838645142dad31b97a02861c60e0bf8364
daniel_zepp poterble <s_u_n_e@naver.com>
2015-12-26 15:35:57 +0900

    editor croppic border bug fix, web title modified, bootstrap css ( about that 'p' ) change
    Former-commit-id: 5e51b0c05f3a6444c8c2b370c2fbf5cc61bfd43a

--- 0.current_lubycon/index.php
+++ 0.current_lubycon/index.php
@@ -13,7 +13,7 @@
     <meta name="copyright" content="copyrights 2015 LUBYCON" />
     <meta name="keywords" content="design, font ,vector, 3D design, community, designers, engineer, 3D printer, Illustration, Lubycon" />
 
-    <title>LUBYCON</title>
+    <title>Lubycon</title>
 
     <link rel="shortcut icon" href="./ch/img/logo/lubycon.ico" /> <!-- favicon -->
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source Sans Pro:200,400" type="text/css" />	<!--google web font-->
