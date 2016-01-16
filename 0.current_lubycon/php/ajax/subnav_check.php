b79571d7ace6d48c5258d6ff928f0db3d1b094a8
daniel_zepp poterble <s_u_n_e@naver.com>
2016-01-16 20:58:35 +0900

    add my forums
    Former-commit-id: 4c4d8b74658641e298840acf839739b389cfa00b

--- 0.current_lubycon/php/ajax/subnav_check.php
+++ 0.current_lubycon/php/ajax/subnav_check.php
@@ -5,7 +5,7 @@ switch($id)
 {
     case 'dashboard':include_once('../personal_page/dashboard.php'); break;
     case 'my_contents':include_once('../personal_page/my_contents.php'); break;
-    case 'my_forum':include_once('../personal_page/my_forum.php'); break;
+    case 'my_forums':include_once('../personal_page/my_forums.php'); break;
     case 'insight':include_once('../personal_page/insight.php'); break;
     case 'bookmark':include_once('../personal_page/bookmark.php'); break;
     case 'account_setting':include_once('../personal_page/account_setting.php'); break;
---
+++ 0.current_lubycon/php/personal_page/my_forums.php
@@ -0,0 +1,8 @@
+<ul>
+    <?php
+        for( $i=0 ; $i<5 ; $i++ )
+        {
+            include("../layout/index_forum.php");
+        };
+    ?>
+</ul>
