4f6328ce77ccfe4b0242bd2c7596e79a904322b7
daniel_zepp poterble <s_u_n_e@naver.com>
2016-02-04 23:21:08 +0900

    in contents view page fill subject, add contents author and index page slider change ( arrow link )
    Former-commit-id: 746514c5015bd1d07f293e2c13f57989970735ee

--- 0.current_lubycon/php/layout/content_card.php
+++ 0.current_lubycon/php/layout/content_card.php
@@ -2,18 +2,17 @@
     <?php
         $number = $_GET["number"];
         $current_url = $third_param;
-        $artwork_subject = array('wild','A.P.C. - Redesign','A4 Magazine Mock Up (FREE)','alp impressions X','Amsdam Typeface','Anyone','Astronaut Y&R Mexico','BEACH HOUSE','Beasts','Blancpain GT Series. Monza (2015)','Cirka Distilleries','Clean Broadcast Package','CLEANSING INSIDE','Climbing crew - Pinch lovers','DAS KREATIVE HAUS','Digital Portraits','Dinette - Restaurant Branding','Donuts Webdesign','driexdrie','Editorial 2015','Exhibition Design Art Fair 2015','Five projects 2015','foodwheels','Frockhub','Greetings','Guide to the Expanse','I-ZUK','Jackpot - Nissan NP300','Kirpichi burger bar','Landyachtz New Graphics','Left March','Lipton Ice Tea','LULA Magazine Website','Manerba Office system catalogues','Marmier','Mechanical fish mod. Delta','MY LAND- Sugar Mountain. #03','Nike - Forward Engineering vs Industrial Logoism','Ofelia','Play','POLO HOSPITALITY','Project Farmhouse Maas Architects','Quartz','Quiet Life','R LOGO II','Revolution','Schmitten Chocolates Splash','SKETCHBOOK HOLIDAYS','Spooky','starwars','Strana detey  Kids world','The King Pirate','This Means War','Trace a Line','Universal Monsters poster set','Vision - Pause fest 2016 Motion Response','Warsteiner X Fricote Magazine','watched','Watts Dominique','Wedding Stationery  Ashley & Edwin','Weekly Project Abstract Shapes','White River');
-        $vector_subject = array('Zombie Selfie','2d graphics','Apex Predator','Arachnoid','BMW E9. Vector illustration','Brave Berserker','Buildings','Buildingss','Carrot App Logo','CBRE Animated Infographics','Character Doodles','Comics Tribute Vector','Cosmos Muertos','Cute Monster Pack','Denmark','Expo 2015 - Cereals & Tubers','Favorite Albums, 2015','Fire temple warriors','Flat Design Manchot','Freddy Krueger SLEEP KILLS (Poster)','FU#K MARVEL','Glennz Tees Concepts & Designs July-Dec 2015','Golden animals','Grayscale Vector','HANDLETTERED LOGOTYPES & MARKS ? VOL. 2','Icarus Myths Legends','Icon Selection 2015','Illusive','Illustration Mix 2013','Jack Nicholson - Low poly','Jon Bellion','Lauren Calaway','Lootcrate - Cyber','Lord Ganesh','Masterok - branding, character, website design','Midnight Girl','My Desk','My vector drawings','Mystic cat force','Natale con i tuoi Christmas Card','Native Americans','Natural Anthem','Owls of the World','Personal tribute to Lemmy Kilmister','Phobos','Polygons Marvel Heroes','Portraits 03','Portraits 04','Queen of Spades','RIDERS','Run Out - Album','Save Us Records Cover Art for EPs 2015','Sea free icons','Shadow of the Colossus Poster Series','Shinobi Systems','Star Wars First order vector','SUPA-ROBOT','Superette - Iconography Collection','Telegramme Paper Co. Wildlife Collection','The New York Times','Timo!','Univali - Video','Vector 15','Vector illustration set','Workshop Grafica vettoriale, la nuova stampa darte','World Landmarks','ZIAMIMI Font');
+
+        include('../contents_data/contents_data.php');
 
         switch($current_url)
         {
-            case "artwork" : $current_url = "artwork"; $contents_name = $artwork_subject; break;
-            case "vector" : $current_url = "vector"; $contents_name = $vector_subject; break;
+            case "artwork" : $current_url = "artwork"; $contents_name = $artwork_subject; $username = $artwork_author;  break;
+            case "vector" : $current_url = "vector"; $contents_name = $vector_subject; $username = $vector_author; break;
             case "3d" : $current_url = "3d"; break;
-            default : $current_url = "artwork"; $contents_name = $artwork_subject; break;
+            default : $current_url = "artwork"; $contents_name = $artwork_subject;$username = $artwork_author; break;
         };
 
-        $username = "Admin_User";
         $price = "Free";
         $contents_thumb_url = "../contents_data/".$current_url."jpg/thumb/".$number.".jpg";
         $user_img_url = "./ch/img/no_img/no_img_user1.jpg";
@@ -47,7 +46,7 @@
                 <a href="./index.php?1=creators_page&2=creators">
                     <img src="<?=$user_img_url?>" class="hidden-mb-ib" alt="artist photo" />
                     <span class="by">by</span>
-                    <span class="name"><?=$username?></span>
+                    <span class="name"><?=$username[$number]?></span>
                 </a>
             </span>
             <i id="bookmark_bt" class="lubyAlert_bt fa fa-star"></i>
